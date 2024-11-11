192cd9afc9398f7af999edf4464e3d3eda621da0
Oleg Osipov <olegish@bk.ru>
2024-11-11 17:30:29 +0300

    add blank for brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,25 @@
+import playGame from '../index.js';
+
+import randomNumber from '../utils.js';
+
+const questionGame = 'What number is missing in the progression?';
+
+const questionRound = () => {
+  let number1 = randomNumber(1, 10);
+  let number2 = randomNumber(1, 10);
+  const question = `Question:${number1} ${number2} ${number2} ${number2}`;
+
+  let result = 0;
+  while (number2 !== 0) {
+    const temp = number2;
+    number2 = number1 % number2;
+    number1 = temp;
+  }
+  result = number1.toString();
+
+  return [question, result];
+};
+
+const gcdGame = () => playGame(questionGame, questionRound);
+
+export default gcdGame;
