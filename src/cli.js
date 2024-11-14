bf19cfc5f0c51e15e4f06218830228f148e22f05
Oleg Osipov <olegish@bk.ru>
2024-11-14 13:58:31 +0300

    cli.js refactoring

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const username = () => {
+function username() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello,${name}!`);
 
   return name;
-};
+}
 
 export default username;
