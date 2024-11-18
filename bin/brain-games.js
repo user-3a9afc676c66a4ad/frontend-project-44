7f38b92d6ad65146af211a21221a06587795fc31
Oleg Osipov <olegish@bk.ru>
2024-11-18 11:27:34 +0300

    brain-games.js refactoring

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 import username from '../src/cli.js';
 
-const greetUser = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = username();
-  console.log(`Hello, ${userName}!`);
-};
-
-export default greetUser;
+console.log('Welcome to the Brain Games!');
+const userName = username();
+console.log(`Hello, ${userName}!`);
