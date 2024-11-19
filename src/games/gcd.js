2bfdce91eb579eb7114f54b86596ba50979c16eb
Oleg Osipov <olegish@bk.ru>
2024-11-19 10:32:40 +0300

    gcd.js refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,7 +7,7 @@ const questionGame = 'Find the greatest common divisor of given numbers.';
 const questionRound = () => {
   let number1 = randomNumber(1, 10);
   let number2 = randomNumber(1, 10);
-  const question = `Question:${number1} ${number2}`;
+  const question = `Question: ${number1} ${number2}`;
 
   let result = 0;
   while (number2 !== 0) {
